073cd242a570b6bd89a5a9b9edb99d6d37c75d29
adithprathap <adithprathap17@gmail.com>
2026-09-23 10:48:07 +0530

    arryreverse.py

---
+++ arryreverse.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the limit: "))
+arry=[]
+for i in range(n):
+  val=int(input("enter the values"))
+  arry.append(val)
+print(arry)
+print("reverse order")
+arry.reverse()
+print(arry)
